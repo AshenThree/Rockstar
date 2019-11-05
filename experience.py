8bb16e0873ac3a481b311dece415462560c808d2
AshenOne <18108235377@163.com>
2019-11-05 13:19:10 +0800

    first commit

---
+++ experience.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Ash
+# @Date:   2019-11-05 13:08:53
+# @Last Modified by:   Ash
+# @Last Modified time: 2019-11-05 13:09:48
+from rockstar import RockStar
+
+vue_code = "print('Hello world')"
+rock_it_bro = RockStar(days=526, file_name='hello.vue', code=vue_code)
+rock_it_bro.make_me_a_rockstar()
